21217024eb8e03dd93d6f5f0d7b43527d00e1127
Tom <tom.weelborg@gmail.com>
2025-10-11 22:25:02 +0200

    feat: add Signal update listening support

--- src/main/java/org/revvactivity/revvactivity_java/signal/signals/Signal.java
+++ src/main/java/org/revvactivity/revvactivity_java/signal/signals/Signal.java
@@ -1,10 +1,18 @@
 package org.revvactivity.revvactivity_java.signal.signals;
 
+import org.revvactivity.revvactivity_java.signal.listeners.SignalUpdateListener;
+import org.revvactivity.revvactivity_java.signal.listeners.SignalChangeListener;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Signal<T> {
     private T value;
+    private final List<SignalChangeListener<? super T>> updateListeners;
 
     protected Signal() {
         super();
+        this.updateListeners = new LinkedList<>();
     }
 
     public Signal(final T value) {
@@ -17,6 +25,21 @@ public class Signal<T> {
     }
 
     protected void setValue(final T value) {
+        final T oldValue = this.value;
         this.value = value;
+
+        notifyUpdateListeners(oldValue);
+    }
+
+    protected void notifyUpdateListeners(final T oldValue) {
+        this.updateListeners.forEach(updateListener -> updateListener.onChange(oldValue, this.value));
+    }
+
+    public void onUpdate(final SignalUpdateListener<? super T> updateListener) {
+        this.updateListeners.add((o, n) -> updateListener.onUpdate(n));
+    }
+
+    public void onUpdate(final SignalChangeListener<? super T> changeListener) {
+        this.updateListeners.add(changeListener);
     }
 }
