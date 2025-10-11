0c2e043886b145f9796f7c57b69d5187d0b5f265
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-10-11 22:32:44 +0200

    feat: add Signal change listening support

--- src/main/java/org/revvactivity/revvactivity_java/signal/signals/Signal.java
+++ src/main/java/org/revvactivity/revvactivity_java/signal/signals/Signal.java
@@ -5,14 +5,17 @@ import org.revvactivity.revvactivity_java.signal.listeners.SignalChangeListener;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class Signal<T> {
     private T value;
     private final List<SignalChangeListener<? super T>> updateListeners;
+    private final List<SignalChangeListener<? super T>> changeListeners;
 
     protected Signal() {
         super();
         this.updateListeners = new LinkedList<>();
+        this.changeListeners = new LinkedList<>();
     }
 
     public Signal(final T value) {
@@ -29,10 +32,17 @@ public class Signal<T> {
         this.value = value;
 
         notifyUpdateListeners(oldValue);
+        if (!Objects.equals(oldValue, value)) {
+            notifyChangeListeners(oldValue);
+        }
     }
 
     protected void notifyUpdateListeners(final T oldValue) {
         this.updateListeners.forEach(updateListener -> updateListener.onChange(oldValue, this.value));
+    }
+
+    protected void notifyChangeListeners(final T oldValue) {
+        this.changeListeners.forEach(changeListener -> changeListener.onChange(oldValue, this.value));
     }
 
     public void onUpdate(final SignalUpdateListener<? super T> updateListener) {
@@ -41,5 +51,13 @@ public class Signal<T> {
 
     public void onUpdate(final SignalChangeListener<? super T> changeListener) {
         this.updateListeners.add(changeListener);
+    }
+
+    public void onChange(final SignalUpdateListener<? super T> updateListener) {
+        this.changeListeners.add((o, n) -> updateListener.onUpdate(n));
+    }
+
+    public void onChange(final SignalChangeListener<? super T> changeListener) {
+        this.changeListeners.add(changeListener);
     }
 }
