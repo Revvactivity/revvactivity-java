5d463a4f57849dbc6f7dca86bce162ce1b89f4de
Tom <tom.weelborg@gmail.com>
2025-10-27 19:37:29 +0100

    refactor: make SignalChangeListener extend SignalListener

--- src/main/java/org/revvactivity/revvactivity_java/signal/listeners/SignalChangeListener.java
+++ src/main/java/org/revvactivity/revvactivity_java/signal/listeners/SignalChangeListener.java
@@ -1,6 +1,6 @@
 package org.revvactivity.revvactivity_java.signal.listeners;
 
 @FunctionalInterface
-public interface SignalChangeListener<T> {
+public interface SignalChangeListener<T> extends SignalListener<T> {
     void onChange(final T oldValue, final T newValue);
 }
