f6e5a32eaa48a05aa70a9c2478ee85b880eff9e0
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-28 23:52:12 +0200

    test: add test for changeValue to WriteSignalTest

--- src/test/java/org/revvactivity/revvactivity_java/signal/signals/WriteSignalTest.java
+++ src/test/java/org/revvactivity/revvactivity_java/signal/signals/WriteSignalTest.java
@@ -1,8 +1,11 @@
 package org.revvactivity.revvactivity_java.signal.signals;
 
 import org.junit.jupiter.api.Test;
+import org.revvactivity.revvactivity_java.signal.listeners.SignalChangeListener;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class WriteSignalTest extends SignalTest {
     @Override
@@ -30,5 +33,45 @@ class WriteSignalTest extends SignalTest {
 
         assertThat(writeSignal.getValue())
                 .isEqualTo(firstValue + appendix);
+    }
+
+    @Test
+    void changeValue() {
+        final WriteSignal<String> writeSignal = new WriteSignal<>();
+
+        final SignalChangeListener<String> changeListener = mock();
+
+        assertThat(writeSignal.getValue())
+                .isNull();
+
+        writeSignal.onChange(changeListener);
+
+        final String firstValue = "Hello";
+
+        writeSignal.changeValue(v -> firstValue);
+
+        assertThat(writeSignal.getValue())
+                .isEqualTo(firstValue);
+
+        verify(changeListener, times(1))
+                .onChange(any(), any());
+
+        writeSignal.changeValue(v -> v);
+
+        assertThat(writeSignal.getValue())
+                .isEqualTo(firstValue);
+
+        verify(changeListener, times(1))
+                .onChange(any(), any());
+
+        final String appendix = ", World!";
+
+        writeSignal.changeValue(v -> v + appendix);
+
+        assertThat(writeSignal.getValue())
+                .isEqualTo(firstValue + appendix);
+
+        verify(changeListener, times(2))
+                .onChange(any(), any());
     }
 }
